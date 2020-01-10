f2ee9dc2854360fdb4ebc451b0b1e85d23c9d54d
Patel <d_patel109038@ats-hr420-dm24.fc.ca>
2020-01-10 14:39:36 -0500

    SecondUpdate

--- js/main.js
+++ js/main.js
@@ -6,4 +6,25 @@
 
   console.log("some message - from javascript");
 
+  // find the button element on the page by its ID with javascript
+  let theButton = document.querySelector("#myButton"),
+  theheading = document.querySelector('h1'),
+  svgImage = document.querySelector('#svgGraphic');
+
+  // this function changes heading text
+  function changeText()
+  {
+    theheading.textContent = "Hello there from the console";
+  }
+
+  function logSvg()
+  {
+    console.log(this.id);
+  }
+
+  theButton.addEventListener("click", changeText);
+  svgImage.addEventListener("mouseover", logSvg);
+  //document.querySelector('h1').textContent = "Hello there from the console";
+
+
 })();
