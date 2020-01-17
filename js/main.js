c6961c39396a6974a2ff1de791c6a2f895ce1057
Patel <d_patel109038@ats-hr420-dm25.fc.ca>
2020-01-17 13:44:21 -0500

    updatedJSFile

--- js/main.js
+++ js/main.js
@@ -7,9 +7,10 @@
   console.log("some message - from javascript");
 
   // find the button element on the page by its ID with javascript
-  let theButton = document.querySelector("#myButton"),
+  let theButton = document.querySelector("#myButton"),  //querySelector = getElementsByClassName('className') = getElementById('id')
   theheading = document.querySelector('h1'),
-  svgImage = document.querySelector('#svgGraphic');
+  // svgImage = document.querySelector('#svgGraphic'),
+  allSVGs = document.querySelectorAll(".svg");
 
   // this function changes heading text
   function changeText()
@@ -23,7 +24,11 @@
   }
 
   theButton.addEventListener("click", changeText);
-  svgImage.addEventListener("mouseover", logSvg);
+  // svgImage.addEventListener("click", logSvg);
+  // svgImage2.addEventListener("click", logSvg);
+
+  // loop through the collection and apply out event handling to each element
+  allSVGs.forEach(item => item.addEventListener('click',logSvg))
   //document.querySelector('h1').textContent = "Hello there from the console";
 
 
