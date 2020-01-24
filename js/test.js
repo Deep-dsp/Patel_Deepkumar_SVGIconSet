373a1e0eb2b34b6bfd03930d43d9c1b35d1af7f5
Patel <d_patel109038@ats-hr420-dm24.fc.ca>
2020-01-24 14:07:54 -0500

    withCss

--- js/test.js
+++ js/test.js
@@ -2,20 +2,16 @@
 {
   //always go and get the elements you are working with.
   const mySpan = document.querySelector('main span'),
-        mySpan2 = document.querySelector('main span');
+        myObject = document.querySelector('.svg-img');
 
   function toggleSelecetion()
   {
     //want to update out UI to make it look like we're making selection
-    this.classList.add("selected"); //add a CSS class to the elemeny with javascript
+    this.classList.toggle("selected"); //add a CSS class to the elemeny with javascript
+    console.log(this.id);
   }
 
-  // function deselect()
-  // {
-  //     this.classList.remove("selected");
-  // }
-
   mySpan.addEventListener('click', toggleSelecetion);
-  // mySpan2.addEventListener('click', deselect)
+  myObject.addEventListener('click', toggleSelecetion);
 
 })();
